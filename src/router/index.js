744f874d6adf1a450a7c26a810c03d6ee61fe137
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-02-09 22:47:34 +0800

    Reattempt to fix 404 issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PokemonView from '../views/PokemonView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/pokemon',
       name: 'pokemon',
-      component: () => import('../views/PokemonView.vue')
+      component: PokemonView//() => import('../views/PokemonView.vue')
     }
   ]
 })
@@ -21,7 +22,8 @@ router.beforeEach((to, from, next) => {
   if (to.path === '/pokemon' && !from.path) {
     next({
       path: '/pokemon',
-      component: () => import('../views/PokemonView.vue')
+      name: 'pokemon',
+      component: PokemonView//() => import('../views/PokemonView.vue')
     })
   } else {
     next()
