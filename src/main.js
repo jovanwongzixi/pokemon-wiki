8f46a90bf0944d7859a4e0615eea72f9d9ad9524
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-02-09 23:03:37 +0800

    Trying out fix

--- src/main.js
+++ src/main.js
@@ -8,4 +8,16 @@ const app = createApp(App)
 
 app.use(router)
 
+router.beforeEach((to, from, next) => {
+    if (to.path === '/pokemon' && !from.path) {
+      next({
+        path: '/pokemon',
+        name: 'pokemon',
+        component: PokemonView//() => import('../views/PokemonView.vue')
+      })
+    } else {
+      next()
+    }
+  })
+
 app.mount('#app')
